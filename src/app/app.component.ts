4155db039407f6b3424b3f834c806b790da3f6dd
MasT3r1k <master0123@centrum.cz>
2022-10-31 18:18:14 +0100

    Dokončený todo list, ukládání dat do localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularTodo';
-  list = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
+  list: Array<string> = [];
+
+  ngOnInit() {
+    localStorage.getItem("Todolist")?.split(',').forEach(item => this.add(item));
+  }
+  
+
+  public saveData(): void {
+    localStorage.setItem("Todolist", this.list.toString());
+  }
+
+  public add(item: string): void {
+    if (!item) return;
+    this.list.push(item);
+    this.saveData();
+  }
+
+  public remove(item: string): void {
+    (this.list.indexOf(item, 0) > -1) ? this.list.splice(this.list.indexOf(item, 0), 1) : console.error("Item nebyl nalezen.");
+    this.saveData();
+
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCommonModule} from '@angular/material/core';
 import { MatListModule } from '@angular/material/list';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import {MatInputModule} from '@angular/material/input';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [
@@ -16,9 +22,15 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatCardModule,
+    MatCommonModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
     MatListModule,
     MatCheckboxModule,
     MatInputModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
